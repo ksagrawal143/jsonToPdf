0c8f56ebb8d3effc21e3bbac7b442eec1833368b
Sahil <55389335+SahilKr24@users.noreply.github.com>
2020-07-15 00:51:15 +0530

    Initial Commit
    Co-authored-by: Rohan Kakar<rkakar2000@gmail.com>

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const createTable = require('./pdf')
+const pdf = require('html-pdf')
+const { response } = require('express')
+const config = require('dotenv').config()
+
+app.use(express.json())
+
+app.post('/getPdf', (request, response) => {
+    try {
+        let req = request.body
+        console.log(req)
+        let html = createTable(req)
+        console.log(html)
+        var options = { width: '56mm' }
+        pdf.create(html, options).toStream((err, stream) => {
+            if (err)
+                return console.log(err)
+            response.status(200)
+            stream.pipe(response)
+        })
+    } catch (error) {
+        console.log(error)
+        response.status(401).send("Error!")
+    }
+})
+
+app.post('/', (request, response) => {
+    console.log(request.body)
+    response.status(200).send("post mil gaya")
+})
+
+app.get('/', (request, response) => {
+    console.log("Hi")
+    response.status(200).send('hi')
+})
+
+const port = process.env.PORT || 8000
+app.listen(port, () => {
+    console.log(`Server started at port ${port}`)
+})
---
+++ pdf.js
@@ -0,0 +1,40 @@
+var pdf = require('html-pdf')
+var fs = require('fs')
+const { table } = require('console')
+var options = { width: '56mm', border: '5mm' }
+
+var html = ""
+
+function getPdf(data) {
+
+    let items = []
+    let totalItems = 0
+    let totalAmt = 0
+
+    data['sno'].forEach((item, index) => {
+        console.log(item)
+        items.push({
+            'sno': item,
+            'item': data['item'][index],
+            'priceu': data['priceu'][index],
+            'qty': data['qty'][index],
+            'gst': data['gst'][index],
+            'amount': data['amount'][index]
+        })
+    })
+
+
+    var table = '<center><table style="text-align:center;width:90%"><thead><tr><th colspan="6">Tandoor Hut</th></tr></thead><tbody><tr></tr><tr></tr><tr><td colspan="6">Godhana Road Ara<br>Phone No. 7279855969<br>Email: tandoorhutata@gmail.com<br>GSTIN:10HIAPK7037L1ZA<br>State: 10-Bihar</td></tr><tr></tr><tr></tr><tr><td colspan="6">TAX INVOICE</td></tr><tr></tr><tr></tr><tr><td colspan="3">Bill To</td><td colspan="2">Invoice No.</td><td>' + data['bill_no'] + '</td></tr><tr><td colspan="3">' + data['bill_to'] + '</td><td colspan="2">Date</td><td>' + data['date'] + '</td></tr><tr><td colspan="6"></td></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr><td>#</td><td colspan=2>Item Name</td><td>Price/U</td><td>Quantity</td><td>Amount</td></tr>'
+    items.forEach(item => {
+        table += `<tr> <td>${item.sno}</td> <td colspan=2>${item.item}</td> <td>${item.priceu}</td> <td>${item.qty}</td><td>${item.amount}</td> </tr>`
+    })
+
+    table += '<tr></tr><tr></tr><tr><td colspan="2">Total</td><td></td><td></td><td>' + data['total_qty'] + '</td><td>' + data['total_amt'] + '</td></tr><tr></tr><tr></tr><tr></tr><tr></tr><tr><td></td><td></td><td></td><td colspan="2">Sub Total</td><td>' + data['subtotal'] + '</td></tr><tr><td colspan="2">Cashier:</td><td>' + data['cashier_name'] + '</td><td colspan="2">CGST</td><td>' + data['cgst'] + '</td></tr><tr><td></td><td></td><td></td><td colspan="2">SGST</td><td>' + data['sgst'] + '</td></tr><tr><td></td><td></td><td></td><td colspan="2">Total</td><td>' + data['total'] + '</td></tr><tr><td></td><td></td><td></td><td colspan="2">Received</td><td>' + data['received'] + '</td></tr><tr><td></td><td></td><td></td><td colspan="2">Balance</td><td>' + data['balance'] + '</td></tr></tbody></table></center>'
+
+
+    html = table
+
+    return html
+}
+
+module.exports = getPdf
